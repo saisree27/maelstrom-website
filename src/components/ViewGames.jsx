9cbcd49de7b9056fe073936317d915f5be841282
saisree27 <saigautambonam@gmail.com>
2022-08-10 22:57:23 -0400

    Add descriptions to board and resize

--- src/components/ViewGames.jsx
+++ src/components/ViewGames.jsx
@@ -3,55 +3,70 @@ import { useInterval } from "../hooks/useInterval";
 
 import "../css/viewgames.css";
 import axios from "axios";
-import { pgnView, pgnBoard, pgnPrint } from '@mliebelt/pgn-viewer';
-
-
+import { pgnView, pgnBoard, pgnPrint } from "@mliebelt/pgn-viewer";
+import { parse } from "@mliebelt/pgn-parser";
 
 export default function ViewGames() {
   const [pgn, setPGN] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [result, setResult] = useState("*");
+  const [white, setWhite] = useState("None");
+  const [black, setBlack] = useState("None");
   const [move, setMove] = useState(0);
-  const id = 'board-' +'breh';
+  const [flip, setFlip] = useState("white");
+
+  const id = "board-" + "breh";
 
   const fetchCurrentPGN = () => {
-    axios.get(
-      "https://lichess.org/api/user/Maelstrom-Chess/current-game", { "evals": false }
-    ).then((res) => {
-      console.log(res);
-      console.log(res.data);
-      setPGN(res.data);
+    axios
+      .get("https://lichess.org/api/user/Maelstrom-Chess/current-game", {
+        evals: false,
+      })
+      .then((res) => {
+        setLoading(false);
+        setPGN(res.data);
 
-      let split = res.data.split("} ");
-      console.log(split);
+        let game = parse(res.data)[0];
 
-      let moveNum = split[split.length - 2];
-      if (moveNum !== undefined) {
-        if (moveNum.includes("...")) {
-          console.log(split[split.length - 2].split("...")[0]);
-          setMove(parseInt(split[split.length - 2].split("...")[0], 10) * 2);
+        setMove(game.moves.length);
+        setResult(game.tags.Result);
+
+        if (game.tags.WhiteTitle == "") {
+          setWhite(game.tags.White);
         } else {
-          console.log(split[split.length - 2].split(".")[0]);
-          setMove(parseInt(split[split.length - 2].split(".")[0], 10) * 2 - 1);
+          setWhite(game.tags.WhiteTitle + " " + game.tags.White);
         }
-      }
-    });
-  }
+
+        if (game.tags.BlackTitle == "") {
+          setBlack(game.tags.Black);
+        } else {
+          setBlack(game.tags.BlackTitle + " " + game.tags.Black);
+        }
+
+        if (game.tags.White == "Maelstrom-Chess") {
+          setFlip("white");
+        } else {
+          setFlip("black");
+        }
+      });
+  };
 
   useLayoutEffect(() => {
     if (pgn != "") {
       console.log(move);
-      pgnView(id,
-        {
-          pgn: pgn,
-          timerTime: '1',
-          locale: 'pl',
-          startPlay: move,
-          showResult: true,
-          layout: "left",
-          colorMarker: true,
-        }
-      )
+      pgnView(id, {
+        pgn: pgn,
+        timerTime: "1",
+        locale: "pl",
+        startPlay: move,
+        showResult: true,
+        layout: "top",
+        colorMarker: true,
+        boardSize: "500",
+        orientation: flip,
+      });
     }
-  })
+  });
 
   useInterval(() => {
     fetchCurrentPGN();
@@ -60,12 +75,22 @@ export default function ViewGames() {
   return (
     <div className="viewgames">
       <div className="live">
-        <h2>Live Match</h2>
+        <h2>Live Game / Most Recent Game</h2>
 
-        <div id={id} className="lichess"></div>
+        <div className="lichess">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <div>
+              <p>
+                {white} (W) vs. {black} (B) {result}
+              </p>
+              <div id={id}></div>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="statsandplay">
-      </div>
+      <div className="statsandplay"></div>
     </div>
   );
 }
