72176aa3e0ea635f3d780ed77511dde0d1d0b30d
saisree27 <saigautambonam@gmail.com>
2022-08-11 17:01:24 -0400

    Add lichess online detection and fetch ratings

--- src/components/ViewGames.jsx
+++ src/components/ViewGames.jsx
@@ -14,8 +14,14 @@ export default function ViewGames() {
   const [black, setBlack] = useState("None");
   const [move, setMove] = useState(0);
   const [flip, setFlip] = useState("white");
+  const [online, setOnline] = useState(false);
 
-  const id = "board-" + "breh";
+  const [blitzRating, setBlitzRating] = useState(0);
+  const [bulletRating, setBulletRating] = useState(0);
+  const [rapidRating, setRapidRating] = useState(0);
+  const [classicalRating, setClassicalRating] = useState(0);
+
+  const id = "board";
 
   const fetchCurrentPGN = () => {
     axios
@@ -51,6 +57,16 @@ export default function ViewGames() {
       });
   };
 
+  const fetchStatistics = () => {
+    axios.get("https://lichess.org/api/user/Maelstrom-Chess").then((res) => {
+      setOnline(res.data.online);
+      setBlitzRating(res.data.perfs.blitz.rating);
+      setRapidRating(res.data.perfs.rapid.rating);
+      setBulletRating(res.data.perfs.bullet.rating);
+      setClassicalRating(res.data.perfs.classical.rating);
+    });
+  };
+
   useLayoutEffect(() => {
     if (pgn != "") {
       console.log(move);
@@ -62,7 +78,7 @@ export default function ViewGames() {
         showResult: true,
         layout: "top",
         colorMarker: true,
-        boardSize: "500",
+        boardSize: "550",
         orientation: flip,
       });
     }
@@ -70,6 +86,7 @@ export default function ViewGames() {
 
   useInterval(() => {
     fetchCurrentPGN();
+    fetchStatistics();
   }, 5000);
 
   return (
@@ -79,18 +96,57 @@ export default function ViewGames() {
 
         <div className="lichess">
           {loading ? (
-            <p>Loading...</p>
+            <p>Loading Game...</p>
           ) : (
             <div>
               <p>
                 {white} (W) vs. {black} (B) {result}
               </p>
               <div id={id}></div>
+              <p>
+                Note: live games will be 3 moves behind realtime on Lichess,
+                which is a feature implemented by the Lichess API to avoid
+                cheating.
+              </p>
             </div>
           )}
         </div>
       </div>
-      <div className="statsandplay"></div>
+      <div className="statsandplay">
+        <h2>Statistics</h2>
+        {loading ? (
+          <p>Loading Statistics...</p>
+        ) : (
+          <div>
+            <div className="status">
+              <a className="currently lichess-link" href="https://lichess.org/@/Maelstrom-Chess"><strong>Maelstrom</strong></a>
+              <p className="currently"> is currently </p>
+              {online ? (
+                <p className="online">online</p>
+              ) : (
+                <p className="offline">offline</p>
+              )}
+              <p className="currently">
+                {online ? " on Lichess." : " on Lichess. Please check back later!"}
+              </p>
+            </div>
+
+            <div className="statistics">
+              <p>Bullet Rating: {bulletRating}</p>
+              <p>Blitz Rating: {blitzRating}</p>
+              <p>Rapid Rating: {rapidRating}</p>
+
+              <p className="currently">View all previous games{' '}</p>
+              <a className="currently lichess-link" href="https://lichess.org/@/Maelstrom-Chess/all">here</a>
+              <p className="currently">.</p>
+            </div>
+          </div>
+        )}
+        <h2>How to Play</h2>
+        <div>
+          <p>When Maelstrom is online on Lichess and not playing a live game, you can challenge it on Lichess! You will need a Lichess account, which is quick and easy to create.</p>
+        </div>
+      </div>
     </div>
   );
 }
